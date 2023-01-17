b3ca16fde525b0e144acf14dc9c27b9c36626ff8
Кирилл Толмачев <stitch00007@gmail.com>
2023-01-18 00:48:58 +0700

    update
    added onboarding

--- Reddit/MyApplication/app/src/main/java/com/blblblbl/myapplication/view/auth/AuthFragment.kt
+++ Reddit/MyApplication/app/src/main/java/com/blblblbl/myapplication/view/auth/AuthFragment.kt
@@ -6,17 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.fragment.app.viewModels
 import com.blblblbl.myapplication.R
@@ -26,15 +29,107 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class AuthFragment : Fragment() {
     private val viewModel: AuthViewModel by viewModels()
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val images = listOf(
+            R.drawable.img1,
+            R.drawable.img2,
+            R.drawable.img3,
+            R.drawable.img4,
+        )
+        val phrases = listOf(
+            getString(R.string.onboarding1),
+            getString(R.string.onboarding2),
+            getString(R.string.onboarding3),
+            getString(R.string.onboarding4)
+        )
         return ComposeView(requireContext()).apply {
             setContent {
-                AuthScreen()
+                val page = remember { mutableStateOf(FIRST_PAGE) }
+                Scaffold(
+                    topBar = { AuthTopBar(
+                        onForwardClicked = {
+                            if (page.value<images.size) page.value++
+                            Log.d("MyLog","page: ${page.value}")},
+                        onBackClicked = {
+                            if (page.value> FIRST_PAGE) page.value--
+                            Log.d("MyLog","page: ${page.value}")},
+                        isBackVisible = page.value> FIRST_PAGE,
+                        isFrontVisible = page.value< images.size
+                    ) }
+                ) {
+                    Surface(modifier = Modifier.padding(top = it.calculateTopPadding())) {
+                        if(page.value<images.size){
+                            val image = images[page.value]
+                            val phrase = phrases[page.value]
+                            onboardingScreen(image,phrase)
+                        }
+                        else{
+                            AuthScreen()
+                        }
+                    }
+                }
             }
         }
+    }
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun AuthTopBar(
+        onForwardClicked: () -> Unit,
+        onBackClicked: () -> Unit,
+        isBackVisible:Boolean,
+        isFrontVisible:Boolean
+    ){
+        TopAppBar(
+            title = {
+                Text(
+                    text = stringResource(id = R.string.onboarding),
+                    color = Color.White
+                )
+            },
+            colors = TopAppBarDefaults.smallTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primary
+            ),
+            navigationIcon = {if (isBackVisible) {
+                IconButton(onClick = { onBackClicked() }) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = stringResource(id = R.string.logout_icon_description),
+                    )
+                }
+            }
+            },
+            actions = { if (isFrontVisible){
+                IconButton(onClick = { onForwardClicked() }) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowForward,
+                        contentDescription = stringResource(id = R.string.logout_icon_description)
+                    )
+                }
+            }
+            }
+        )
+    }
+    @Composable
+    fun onboardingScreen(image:Int,phrase:String){
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painterResource(image),
+                contentDescription = ""
+            )
+            Text(text = phrase)
+        }
+
     }
     @Composable
     fun AuthScreen(){
@@ -50,5 +145,8 @@ class AuthFragment : Fragment() {
             }
         }
     }
-
+    companion object{
+        const val FIRST_PAGE:Int = 0
+        const val LAST_PAGE:Int = 4
+    }
 }
